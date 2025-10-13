9e1bfecaa4ab62ee01ea18b96dfbebc017ee1c1e
Drona Bopche <161079263+dronabopche@users.noreply.github.com>
2025-10-13 15:28:07 +0530

    Add files via upload

---
+++ week6/PRINT.java
@@ -0,0 +1,27 @@
+import java.io.reader;
+
+//use bufferreader and readline() for implemention of print function.
+
+ TryCatchBlock()
+    {
+        try
+        {
+            
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+public class PRINT
+{
+	
+	scanf()
+	{
+	
+	}
+ public static void main(String[] args) {
+        // code application logic here
+    }
+} 
